2696a1d8eeedd69fb9b0e210e7f4c55c48334d5e
Vidhan <vidhansharma01@gmail.com>
2026-01-31 21:42:14 +0530

    Added redis dependency

---
+++ src/test/java/com/redis_bookmyshow/show/ShowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redis_bookmyshow.show;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
